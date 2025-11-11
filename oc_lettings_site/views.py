23039d58eefb2035bb80b5f203a63e1dff590aa4
MagNott <notteghem.magali@gmail.com>
2025-11-11 18:43:43 +0100

    #5 Add custom error handlers and templates for 404 and 500 errors

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -14,3 +14,30 @@ def index(request: HttpRequest) -> HttpResponse:
     """
 
     return render(request, 'index.html')
+
+
+def custom_404(request: HttpRequest, exception) -> HttpResponse:
+    """
+    Custom 404 error handler.
+
+    Args:
+        request (HttpRequest): The HTTP request sent by the user.
+        exception: The exception that triggered the 404 error.
+
+    Returns:
+        HttpResponse: The rendered HTML response for the 404 error page.
+    """
+    return render(request, '404.html', status=404)
+
+
+def custom_500(request: HttpRequest) -> HttpResponse:
+    """
+    Custom 500 error handler.
+
+    Args:
+        request (HttpRequest): The HTTP request sent by the user.
+
+    Returns:
+        HttpResponse: The rendered HTML response for the 500 error page.
+    """
+    return render(request, '500.html', status=500)
